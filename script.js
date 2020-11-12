35538011987c00fb7278fe60a6600b077d8fb1c6
Malin Wedin Bäckman <malinwedinbackman@MacBook-Pro-3.lan>
2020-11-12 18:00:34 +0100

    update dropdown, click, css.

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
-const header= document.querySelector('header')
-const dropdown = document.getElementById('toggleThis');
+window.addEventListener('load', main);
 
-dropdown.onclick = function() {
-    if (header.style.height == '100%') {
-        header.style.height = null;
-    } else {
-        header.style.height = '100%';
+function main() {
+    headerDropDownMenu();
+    headerDropDownMenuUp();
+}
+
+function headerDropDownMenu() {
+    const header = document.querySelector('header')
+    const dropdown = document.getElementById('toggleThis');
+
+    dropdown.onclick = function() {
+        if (header.style.height == '100%') {
+            header.style.height = null;
+        } else {
+            header.style.height = '100%';
+        }
     }
+    
+}
+
+function headerDropDownMenuUp() {
+    const header = document.querySelector('header')
+    const linkClicked = document.getElementById('afterClick');
+
+    linkClicked.onclick = function()  {
+        if (header.style.height == '100%') {
+            header.style.height = null;
+        } else {
+            header.style.height = '100%';
+        }
+    }
+    
 }
