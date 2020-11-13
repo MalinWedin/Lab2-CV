c6aad97f70e42228b9ed2b141e9b591b4968b24d
Malin Wedin Bäckman <malinwedinbackman@MacBook-Pro-3.lan>
2020-11-13 10:24:11 +0100

    dropdown links

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ window.addEventListener('load', main);
 function main() {
     headerDropDownMenu();
     headerDropDownMenuUp();
+    headerDropDownMenuUp2();
+    headerDropDownMenuUp3();
+    headerDropDownMenuUp4();
 }
 
 function headerDropDownMenu() {
@@ -23,12 +26,50 @@ function headerDropDownMenuUp() {
     const header = document.querySelector('header')
     const linkClicked = document.getElementById('afterClick');
 
-    linkClicked.onclick = function()  {
+    linkClicked.onclick = function() {
+        if (header.style.height == '100%') {
+            header.style.height = null;
+        } else {
+            header.style.height = '100%';
+        }
+    }    
+}
+
+function headerDropDownMenuUp2() {
+    const header = document.querySelector('header')
+    const linkClicked = document.getElementById('afterClick2');
+
+    linkClicked.onclick = function() {
         if (header.style.height == '100%') {
             header.style.height = null;
         } else {
             header.style.height = '100%';
         }
     }
-    
+}
+
+function headerDropDownMenuUp3() {
+    const header = document.querySelector('header')
+    const linkClicked = document.getElementById('afterClick3');
+
+    linkClicked.onclick = function() {
+        if (header.style.height == '100%') {
+            header.style.height = null;
+        } else {
+            header.style.height = '100%';
+        }
+    }
+}
+
+function headerDropDownMenuUp4() {
+    const header = document.querySelector('header')
+    const linkClicked = document.getElementById('afterClick4')
+
+    linkClicked.onclick = function() {
+        if (header.style.height == '100%') {
+            header.style.height = null;
+        } else {
+            header.style.height = '100%'
+        }
+    }
 }
